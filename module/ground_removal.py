fix: Replace average distance with precise distance

module/ground_removal.py
@@ -112,20 +112,16 @@ class Processor:
 
             label[slice_list[i]:slice_list[i + 1]] = non_ground == 0
 
-        #vel_non_ground = point5D[label == 1][:, :3]
         vel_non_ground = point5D[label == 1]
 
         return vel_non_ground
     
     def Ground_offset(self, point5D):
-        label = np.zeros([point5D.shape[0]])
-        slice_list = np.r_[np.nonzero(np.r_[1, np.diff(point5D[:, 3])])[0], len(point5D)]
 
         for i, seg_idx in enumerate(self.seg_list):
             segment = self.segments[i]
             point5D_seg = point5D[point5D[:, 3] == seg_idx]
 
-            z_min = point5D_seg[:,2].min()
             offset = segment.verticalDistanceToLine_offset(point5D_seg[:, [4, 2]])  # x,y -> d,z
             point5D[point5D[:, 3] == seg_idx, 2] = offset
 
@@ -225,16 +221,23 @@ class Segmentation:
         return label
 
     def verticalDistanceToLine_offset(self, xy):  # checked
-        kMargin = 0.1
-        offset = np.zeros(len(xy))
+        offset = xy[:,1]
 
-        for d_l, d_r, m, b in self.lines:
-            distance = xy[:,1] - (m * xy[:,0] + b)
-            distance = np.clip(distance,0,None)
+        if len(self.lines) >= 1:
+            # Find the index corresponding to the range of lines
+            xy_index = xy[:,0:1] @ np.array([[1]*len(self.lines)*2])
+            xy_index = np.abs(xy_index - np.array(self.lines)[:,:2].flatten())
+            #xy_index = np.argmin(xy_index,axis = 1)//2 # first min index
+            xy_index = (2*len(self.lines)-1 - np.argmin(xy_index[:,::-1],axis = 1))//2 # last min index
 
-            offset += distance
+            for i,(d_l, d_r, m, b) in enumerate(self.lines):
+                xy_i = xy[xy_index == i] #points in lines[i] range
+                if len(xy_i) == 0:
+                    continue
+                distance = xy_i[:, 1] - (m * xy_i[:, 0] + b)
+                offset[xy_index == i] = distance
 
-        return offset/len(self.lines)
+        return offset
 
     def fitSegmentLines(self, min_z):
         cur_line_points = [min_z[0]]